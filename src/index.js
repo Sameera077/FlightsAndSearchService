fabe5a5462042d16216aaaec9ffecc5e16d93a9a
Sameera077 <sameerashaik1022@gmail>
2025-01-17 18:22:02 +0530

    setup basic dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 //Database config setup
+const express = require('express');
+
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async() => {
+    //create the express object
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+    })
+}
+
+setupAndStartServer();
