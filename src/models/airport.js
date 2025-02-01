5d0cd4489a70824cc199801a6a9ccf91e39afdd6
Sameera077 <sameerashaik1022@gmail>
2025-02-01 10:31:18 +0530

    Normalize line endings

---
+++ src/models/airport.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Airport extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.City, {
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE' //the moment u delete a xcity, at that point of time only, all the airports associated with that city gets deleted
+      })
+    }
+  }
+  Airport.init({
+    name: {
+       type: DataTypes.STRING,
+        allowNull:false
+      },
+    address: DataTypes.STRING,
+    cityId: {
+      type: DataTypes.INTEGER,
+       allowNull: false
+  }
+  }, {
+    sequelize,
+    modelName: 'Airport',
+  });
+  return Airport;
+};
