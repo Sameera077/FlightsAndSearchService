dacae8367f54e3d70a30b7c95f73b7ed62b63960
Sameera077 <sameerashaik1022@gmail>
2025-02-01 19:08:50 +0530

    Added flights create crud

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,9 @@
 // Module Aggregation: It exports services from city-service.js.
 // This allows other parts of the application to import all services from a single entry point, simplifying the import statements.
 
+const FlightService = require('./flight-service');
+
 module.exports = {
-    CityService: require('./city-service')
+    CityService: require('./city-service'),
+    FlightService: require('./flight-service')
 }
